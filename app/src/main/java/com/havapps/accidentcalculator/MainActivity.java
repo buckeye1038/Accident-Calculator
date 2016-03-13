69c81bfaefe72c002349382cb351dbea009b98db
John Haviland <haviland.10@osu.edu>
2016-03-13 17:54:41 -0400

    full project

--- app/src/main/java/com/havapps/accidentcalculator/MainActivity.java
+++ app/src/main/java/com/havapps/accidentcalculator/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
